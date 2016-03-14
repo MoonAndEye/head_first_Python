68586b254875d9fd18d64697ee6b56925c7095f6
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-03-14 09:44:02 +0900

    完成一次呼叫生出字典

--- ch6.py
+++ ch6.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 13 22:47:28 2016
-
 @author: Moon
 明天從這個檔案開始改起
 在p185,這裡有四個要求
@@ -25,7 +24,12 @@ def get_coach_data(filename):
         with open(filename) as file:
             data = file.readline()
         files = data.strip().split(',')
-        return (files)
+        diction = {}
+        diction['Name'] = files.pop(0)
+        diction['DOB'] = files.pop(0)
+        diction['time'] = files
+        diction['printout'] = str(sorted(set([sanitize(t) for t in files]))[0:3])
+        return (diction) 
         
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
@@ -35,19 +39,19 @@ def get_coach_data(filename):
         
 sarah = get_coach_data('sarah2.txt')
 (sarah_name, sarah_dob) = sarah.pop(0), sarah.pop(0)
-
 print(sarah_name + "'s fastest times are: " + str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
 """
 
-sarah_list = get_coach_data('sarah2.txt')
+sarah_dict = get_coach_data('sarah2.txt')
 
+"""
 sarah = {}
 
 sarah['Name'] = sarah_list.pop(0)
 sarah['DOB'] = sarah_list.pop(0)
 
 sarah['result']=sarah_list
-
-print(str(sarah['Name']) + 
+"""
+print(str(sarah_dict['Name']) + 
 "'s fastest times are: " + 
-str(sorted(set([sanitize(t) for t in sarah_list]))[0:3]))
+str(sarah_dict['printout']))
