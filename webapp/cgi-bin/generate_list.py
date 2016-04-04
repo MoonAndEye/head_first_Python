7273b835fea9205474f7355ad1fd807fac332cc0
Moonandeye <atimis19@gmail.com>
2016-04-04 23:36:15 +0900

    一直有 syntax error 解不掉
    最後直接copy程式碼
    p230和p246可能有地方沒寫對

--- webapp/cgi-bin/generate_list.py
+++ webapp/cgi-bin/generate_list.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  3 23:34:36 2016
+#! /usr/local/bin/python3
 
-@author: Moon
-"""
+import glob
+
 import athletemodel
 import yate
-import glob
 
 data_files = glob.glob("data/*.txt")
 athletes = athletemodel.put_to_store(data_files)
@@ -14,10 +11,8 @@ athletes = athletemodel.put_to_store(data_files)
 print(yate.start_response())
 print(yate.include_header("Coach Kelly's List of Athletes"))
 print(yate.start_form("generate_timing_data.py"))
-print(yate.para("Select ana athlete from the list to work with:"))
-
+print(yate.para("Select an athlete from the list to work with:"))
 for each_athlete in athletes:
-    print(yate.radio_button("Which_athlete", athletes[each_athlete].name))
-    
+    print(yate.radio_button("which_athlete", athletes[each_athlete].name))
 print(yate.end_form("Select"))
 print(yate.include_footer({"Home": "/index.html"}))
--- webapp/cgi-bin/generate_timing_data.py
+++ webapp/cgi-bin/generate_timing_data.py
@@ -1,20 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  3 23:53:48 2016
-
-@author: Moon
-"""
-
 import cgi
+
+import cgitb
+cgitb.enable()
+
 import athletemodel
 import yate
-import glob
+
+athletes = athletemodel.get_from_store()
 
 form_data = cgi.FieldStorage()
 athlete_name = form_data['which_athlete'].value
 
 print(yate.start_response())
-print(yate.include_header("Best time data"))
-
-print(yate.para("Time data for " + athlete_name.name))
-
+print(yate.include_header("Coach Kelly's Timing Data"))    
+print(yate.header("Athlete: " + athlete_name + ", DOB: " +
+                      athletes[athlete_name].dob + "."))
+print(yate.para("The top times for this athlete are:"))
+print(yate.u_list(athletes[athlete_name].top3))
+print(yate.include_footer({"Home": "/index.html",
+                           "Select another athlete": "generate_list.py"}))
